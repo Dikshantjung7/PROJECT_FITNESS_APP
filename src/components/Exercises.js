de227fb3f0cea8a90bdf4c1f60b4961e7c0ffc3b
Dikshantjung7 <dikshantkhadka2003@gmail.com>
2023-12-09 00:04:14 -0500

    exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -17,7 +17,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       if (bodyPart === 'all') {
         exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
       } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/back${bodyPart}`, exerciseOptions);
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPartList${bodyPart}`, exerciseOptions);
       }
 
       setExercises(exercisesData);
